5f1f06518b8f1eb5219faed92446f219d2f6b692
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2024-04-10 16:45:45 -0400

    Change version strings, output file names

--- conda-recipes/build_sigprofilerplotting.sh
+++ conda-recipes/build_sigprofilerplotting.sh
@@ -5,11 +5,13 @@ source /home/ljl11/miniconda3_for_building/bin/activate
 grayskull pypi --strict-conda-forge sigprofilerplotting
 
 # Add numpy < 1.23.0 requirement (currently numpy is 1.23.1, causes scipy to fail)
-awk '{ if ($0 ~ /- python/) { print $0; print "    - numpy <1.23.0"; } }' sigprofilerplotting/meta.yaml > sigprofilerplotting/tmpmeta.yaml
-mv sigprofilerplotting/tmpmeta.yaml sigprofilerplotting/meta.yaml 
+#awk '{ if ($0 ~ /- python/) { print $0; print "    - numpy <1.23.0"; } }' sigprofilerplotting/meta.yaml > sigprofilerplotting/tmpmeta.yaml
+#mv sigprofilerplotting/tmpmeta.yaml sigprofilerplotting/meta.yaml 
+
+cp BSD_2_CLAUSE_LICENSE sigprofilerplotting/PLEASE_ADD_LICENSE_FILE
 
 
 conda-build -c conda-forge -c bioconda sigprofilerplotting
 
 
-anaconda upload     /home/ljl11/miniconda3_for_building/conda-bld/noarch/sigprofilerplotting-1.2.2-py_0.tar.bz2
+anaconda upload     /home/ljl11/miniconda3_for_building/conda-bld/noarch/sigprofilerplotting-1.3.14-py_0.tar.bz2
