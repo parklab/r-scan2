db6d13dd20178689847a74c84b3b6aeebfaa2927
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2024-04-23 02:37:36 -0400

    Fix typo

--- R/summary.R
+++ R/summary.R
@@ -96,7 +96,7 @@ make.summary.scan2 <- function(object, preserve.object=TRUE, quiet=FALSE) {
         # spatial.sensitivity$data and binned.counts) to save memory before
         # multithreading.
         call.mutations.and.mutburden=summarize.call.mutations.and.mutburden(object, preserve.object=preserve.object, quiet=quiet),
-        mutsig.rescue=object@mutsig
+        mutsig.rescue=object@mutsig.rescue
     )
 
     gatk.summary <- summarize.gatk(object, quiet=quiet)
